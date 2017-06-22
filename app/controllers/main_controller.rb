ac9eb29d5ddbc3dcf7d07c408a33f1013fe832b7
Paul Krause <p.krause@surrey.ac.uk>
2017-06-22 23:51:16 +0100

    a bit more styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,11 +3,14 @@ class MainController < ApplicationController
   end
 
   def about
+    @title = "About"
   end
 
   def sales
+    @title = "Sales"
   end
 
   def contact
+    @title = "Contact"
   end
 end
